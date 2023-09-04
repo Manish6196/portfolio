dbc186c75d11805ab4d68eb8cb627a69c0664706
manish <manishrajput6196@gmail.com>
2023-09-04 08:24:45 +0530

    feat: portfolio

---
+++ src/constants/index.js
@@ -0,0 +1,258 @@
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  docker,
+  meta,
+  starbucks,
+  tesla,
+  shopify,
+  carrent,
+  jobit,
+  tripguide,
+  threejs,
+  lanetLogo,
+  airbnbLogo,
+} from '../assets'
+
+export const navLinks = [
+  {
+    id: 'about',
+    title: 'About',
+  },
+  {
+    id: 'work',
+    title: 'Work',
+  },
+  {
+    id: 'contact',
+    title: 'Contact',
+  },
+]
+
+const services = [
+  {
+    title: 'Frontend (React, Next, Vue)',
+    icon: web,
+  },
+  // {
+  //   title: "React Native Developer",
+  //   icon: mobile,
+  // },
+  {
+    title: 'Backend (Next, Node, Express)',
+    icon: backend,
+  },
+  {
+    title: 'DevOps (Docker, CI/CD)',
+    icon: creator,
+  },
+]
+
+const technologies = [
+  {
+    name: 'HTML 5',
+    icon: html,
+  },
+  {
+    name: 'CSS 3',
+    icon: css,
+  },
+  {
+    name: 'JavaScript',
+    icon: javascript,
+  },
+  {
+    name: 'TypeScript',
+    icon: typescript,
+  },
+  {
+    name: 'React JS',
+    icon: reactjs,
+  },
+  {
+    name: 'Redux Toolkit',
+    icon: redux,
+  },
+  {
+    name: 'Tailwind CSS',
+    icon: tailwind,
+  },
+  {
+    name: 'Node JS',
+    icon: nodejs,
+  },
+  {
+    name: 'MongoDB',
+    icon: mongodb,
+  },
+  {
+    name: 'Three JS',
+    icon: threejs,
+  },
+  {
+    name: 'git',
+    icon: git,
+  },
+  {
+    name: 'figma',
+    icon: figma,
+  },
+  {
+    name: 'docker',
+    icon: docker,
+  },
+]
+
+const experiences = [
+  {
+    title: 'Web Development Trainee',
+    company_name: 'La Net Team',
+    icon: lanetLogo,
+    iconBg: '#383E56',
+    date: 'April 2019 - October 2019',
+    points: [
+      'I started my web development journey with guidance from experienced mentors.',
+      'I learned HTML, CSS, and JavaScript and also got hands-on experience with tools like Git.',
+      'I became skilled in creating interactive front-end designs using React.js and Vue.js, and I also explored back-end technologies like Node.js and Express.js.',
+      'This training provided me with a solid base to excel in my future roles.',
+    ],
+  },
+  {
+    title: 'Full Stack Developer',
+    company_name: 'La Net Team',
+    icon: lanetLogo,
+    iconBg: '#383E56',
+    date: 'October 2019 - October 2021',
+    points: [
+      'Developing and maintaining web applications using React.js and other related technologies.',
+      'Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.',
+      'Implementing responsive design and ensuring cross-browser compatibility.',
+      'Participating in code reviews and providing constructive feedback to other developers.',
+    ],
+  },
+  {
+    title: 'Sr. Full stack Developer',
+    company_name: 'La Net Team',
+    icon: lanetLogo,
+    iconBg: '#E6DEDD',
+    date: 'October 2021 - Present',
+    points: [
+      'Excelling in the role of a Senior React Developer, taking charge of elevating user experiences through front-end leadership.',
+      'My expertise in React and Node enables me to create sophisticated and intuitive applications.',
+      'I also mentor fellow team members, fostering collaboration and best practices.',
+      'Through strategic insights and technical finesse, I consistently contribute to delivering exceptional, user-centered project outcomes.',
+    ],
+  },
+]
+
+const testimonials = [
+  {
+    testimonial:
+      'I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.',
+    name: 'Sara Lee',
+    designation: 'CFO',
+    company: 'Acme Co',
+    image: 'https://randomuser.me/api/portraits/women/4.jpg',
+  },
+  {
+    testimonial:
+      "I've never met a web developer who truly cares about their clients' success like Rick does.",
+    name: 'Chris Brown',
+    designation: 'COO',
+    company: 'DEF Corp',
+    image: 'https://randomuser.me/api/portraits/men/5.jpg',
+  },
+  {
+    testimonial:
+      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+    name: 'Lisa Wang',
+    designation: 'CTO',
+    company: '456 Enterprises',
+    image: 'https://randomuser.me/api/portraits/women/6.jpg',
+  },
+]
+
+const projects = [
+  {
+    name: 'Property Rent',
+    description:
+      'Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.',
+    tags: [
+      {
+        name: 'nextjs',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'tailwind',
+        color: 'pink-text-gradient',
+      },
+      {
+        name: 'mongodb',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'prisma',
+        color: 'blue-text-gradient',
+      },
+    ],
+    image: airbnbLogo,
+    source_code_link: 'https://github.com/Manish6196/airbnb',
+  },
+  {
+    name: 'Job IT',
+    description:
+      'Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.',
+    tags: [
+      {
+        name: 'react',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'restapi',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'scss',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: jobit,
+    source_code_link: 'https://github.com/',
+  },
+  {
+    name: 'Trip Guide',
+    description:
+      'A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.',
+    tags: [
+      {
+        name: 'nextjs',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'supabase',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'css',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: tripguide,
+    source_code_link: 'https://github.com/',
+  },
+]
+
+export { services, technologies, experiences, testimonials, projects }
